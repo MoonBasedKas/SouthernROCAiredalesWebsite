f6ce20694312ef5dc1a1668db9913fbd8ce785c2
MoonBasedKas <kassidy.maberry@gmail.com>
2025-06-19 15:44:31 -0600

    Made gitignore

--- app/app.py
+++ app/app.py
@@ -1,14 +1,7 @@
 from flask import Flask
 from flask import render_template
-from models import dbCon, main, paillierObj, paillerKeys
-
-
-
-
-
 from flask import Flask, jsonify, request
-keys = paillerKeys.paillerKeys() # Update this
-db = dbCon.DBCon() # Connect to the database
+
 app = Flask(__name__)
 
 
