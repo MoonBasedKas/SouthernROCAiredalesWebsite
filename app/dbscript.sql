c723bfc4bc978d6d44446c632d70d74a92ba7636
MoonBasedKas <kassidy.maberry@gmail.com>
2025-06-24 16:23:35 -0600

    Write temp values

--- app/dbscript.sql
+++ app/dbscript.sql
@@ -1,7 +1,9 @@
+-- Table creation
 CREATE TABLE dogs (
     id int not null auto_increment,
     name char 256,
     gender boolean,
+    available boolean,
     desc char 4096,
     primary key (id)
 );
@@ -12,4 +14,15 @@ create Table images(
     photoName char 256,
     primary key (id),
     foreign key (dogId) references dogs(id)
-)
+);
+
+-- Temp values
+insert into dogs (name, gender, available, desc) values (
+    "Jerry", 1, 1, "Jerry is a dog that goes beyond what is possible for all dogs. Some say Jerry isn't a dog but I say Jerry is the dog."
+    "Jerrette", 1, 1, "Jerrette is Jerry, but if they were a female dog instead."
+);
+
+insert into images (dogId, photoName) values(
+    1, "Jerry1.png"
+    2, "Jerrette1.png"
+);
