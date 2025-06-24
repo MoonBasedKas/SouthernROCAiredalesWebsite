c6e8f4d80cd74cdcfe06260d64d1721d2d9b517e
MoonBasedKas <kassidy.maberry@gmail.com>
2025-06-23 19:25:07 -0600

    Added basic rendering functions for top menu items

--- app/app.py
+++ app/app.py
@@ -15,3 +15,33 @@ app = Flask(__name__)
 def Welcome():
     return render_template('home.html')
 
+
+"""
+For viewing all the dogs
+"""
+@app.route('/dogs')
+def dogs():
+    return render_template('dogs.html')
+
+"""
+I don't remember what this page was supposed to be./
+"""
+@app.route('/health_gaurantee')
+def health():
+    return render_template('health.html')
+
+
+"""
+For rendering the about us
+"""
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+"""
+For rendering the contact us
+"""
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
