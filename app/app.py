2743904cdba564184e166541a5a6bb81dc8df9e8
MoonBasedKas <kassidy.maberry@gmail.com>
2025-07-10 01:01:27 -0600

    Import some password things and update user counter

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
-from flask import render_template
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, session, url_for, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
 import pandas
 import numpy as np
 # import dbController
+
+restrictedPages = {}
 
 # db = dbController.dbController()
 
@@ -22,8 +24,30 @@ try:
 except:
     photos=pandas.DataFrame(columns=["id", "dogID", "photoName"])
     
+# App Config    
 app = Flask(__name__)
+app.secret_key = "A Super Secret Key"
 
+
+
+
+# Config of SQL alchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 # @app.route('/home')
 # def Welcome(name = None):
@@ -34,11 +58,22 @@ app = Flask(__name__)
 @app.route('/index')
 def Welcome():
     global counter
-    counter += 1
+
+        
     available = dogDB[dogDB["available"] == True]
     
     males = available[available["gender"] ==  True]
     females = available[available["gender"] ==  False]
+
+    # Set Cookies
+    visit = request.cookies.get("visited")
+    if visit != "true":
+        counter += 1
+    else:
+        resp = make_response(render_template('home.html', males=males.values.tolist(), females=females.values.tolist(), count=counter))
+        resp.set_cookie(key="visited", value="true", max_age=90*60*60*24)
+        return resp
+
     return render_template('home.html', males=males.values.tolist(), females=females.values.tolist(), count=counter)
 
 
@@ -148,7 +183,32 @@ For rendering the contact us
 """
 @app.route('/contact')
 def contact():
+    if "username" in session:
+        return "yes"
     return render_template('contact.html')
 
+
+
+
+# Login Magic
+# Remember to use do @login_required
+# def login_required(secrets, funcName):
+#     if "username" not in session:
+#         return redirect(url_for("home"))
+#     return secrets[funcName]
+
+# Protected pages
+@app.route("/secret")
+def secret():
+    if "username" not in session:
+        return redirect(url_for("Welcome"))
+    return "Secret Key"
+
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, Threading=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
